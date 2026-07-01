d68526fb368de298f5927452baef039886d458fa
sr1068 <sidreed34@gmail.com>
2026-07-01 14:30:44 -0400

    first attempt at generalizing gene association testing code across associations across Hi-C features and association types

--- scripts/Delta.Expression.Association.Testing/test.HiF.DEG.functional.associations.R
+++ scripts/Delta.Expression.Association.Testing/test.HiF.DEG.functional.associations.R
@@ -49,25 +49,58 @@ test.hyper.params.df <-
 # Load Gene & HiF annotations
 ######################################################################
 all.HiF.DEG.mappings.df <- 
+    # list_all_per_condition_gene_association_HiF_mapping_results() %>% 
     HIF_GENE_ASSOCIATION_MAPPING_DIR %>%
     parse_results_filelist(
-        filename.column.name='SampleID',
-        suffix='-HiF.Gene.Associations.tsv'
+        suffix='-HiF.Gene.Associations.tsv',
+        filename.column.name='SampleID'
     ) %>%
+    filter(
+        HiF.type %in% c(
+            'loop',
+            'compartment.region'
+        )
+    ) %>% 
+        {.} -> tmp; tmp;
+        tmp %>% 
+        head(10) %>% 
     mutate(
-        HiF.DEG.associations.df=
+        test.results=
             pmap(
-                .l=list(filepath),
-                .f=read_tsv,
-                show_col_types=FALSE,
-                progress=FALSE,
+                .l=.,
+                .f=compute_all_test_results,
                 .progress=TRUE
             )
-    ) %>%
-        {.} -> tmp; tmp
-        tmp %>%
-        cross_join(tibble(test.type=c('fisher'))) %>% 
-        cross_join(tibble(nearby.dist.thresh.bins=c(1))) %>% 
+    )
+compute_all_test_results <- function(
+    filepath,
+    HiF.type,
+    ...){
+    # paste('row.index=1', paste0(colnames(tmp), '=tmp$', colnames(tmp), '[[row.index]]', collapse='; '), 'tmp %>% head(row.index) %>% tail(1) %>% t()', sep='; ')
+    # row.index=1; filepath=tmp$filepath[[row.index]]; association.type=tmp$association.type[[row.index]]; association.subtype=tmp$association.subtype[[row.index]]; association.source=tmp$association.source[[row.index]]; association.strategy=tmp$association.strategy[[row.index]]; resolution=tmp$resolution[[row.index]]; HiF.type=tmp$HiF.type[[row.index]]; HiF.scope=tmp$HiF.scope[[row.index]]; SampleID=tmp$SampleID[[row.index]]; tmp %>% head(row.index) %>% tail(1) %>% select(-c(filepath)) %>% t()
+    filepath %>% 
+    read_tsv(show_col_types=FALSE, progress=FALSE) %>%
+        {.} -> tmp2; tmp2
+    tmp2 %>% 
+    pivot_wider(names_from=feature.y, values_from=value.y)
+    tmp2 %>% colnames()
+    tmp2 %>% count(!is.na(FeatureID))
+    # tmp2 %>% count(feature.x, feature.y)
+    tmp2 %>% 
+        select(
+            # SampleID.Numerator, SampleID.Denominator,
+            association.status,
+            Sample.Group.Numerator, Sample.Group.Denominator,
+            FeatureID, chr, start, end,
+            compartment,
+            ends_with('.DESeq2')
+        ) %>%
+        distinct() %>%
+        count(padj.DESeq2 < 0.1, association.status, !is.na(FeatureID))
+}
+        cross_join(tibble(test.type=c('t.test'))) %>% 
+        # cross_join(tibble(test.type=c('t.test', 'fisher'))) %>% 
+        # cross_join(tibble(nearby.dist.thresh.bins=c(1))) %>% 
         # cross_join(tibble(nearby.dist.thresh.bins=c(1, 2, 3))) %>% 
     calculate_all_association_test_results(
         p.corr.group.cols=
@@ -80,4 +113,8 @@ all.HiF.DEG.mappings.df <-
             )
     ) %>%
     select(association.subtype, resolution, SampleID, test.p.value)
+
+# all.between.condition.HiF.DEG.mappings.df <- 
+#     list_all_bewteen_condition_gene_association_HiF_mapping_results() %>% 
+#         {.}
 
